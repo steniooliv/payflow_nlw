46bf8a69e0f8b00e02e6f8645b06cd45186fc647
Stenio Oliveira <steniooliv@gmail.com>
2021-06-21 23:58:55 -0300

    create colors, images and text app theme

---
+++ lib/shared/themes/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static final primary = Color(0xFF1AFF68);
+  static final grey = Color(0xFF585666);
+  static final delete = Color(0xFFE83F5B);
+  static final heading = Color(0xFF585666);
+  static final body = Color(0xFF706E7A);
+  static final inputs = Color(0xFFB1B0B8);
+  static final stroke = Color(0xFFE3E3E5);
+  static final shape = Color(0xFFFAFAFC);
+  static final background = Color(0xFFFFFFFF);
+}
