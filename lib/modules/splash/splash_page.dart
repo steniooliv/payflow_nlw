cb5849ebae83725e1e5ce973acb5fefe3db6e215
Stenio Oliveira <steniooliv@gmail.com>
2021-06-21 23:59:03 -0300

    create splash page

---
+++ lib/modules/splash/splash_page.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:payflow_nlw/shared/themes/app_colors.dart';
+import 'package:payflow_nlw/shared/themes/app_images.dart';
+
+class SplashPage extends StatelessWidget {
+  const SplashPage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: AppColors.background,
+      body: Stack(
+        children: [
+          Center(
+            child: Image.asset(AppImages.union),
+          ),
+          Center(
+            child: Image.asset(AppImages.logoFull),
+          ),
+        ],
+      ),
+    );
+  }
+}
