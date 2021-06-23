e56303d4f2e83cb507784eaa3897836a3fa8df31
Stenio Oliveira <steniooliv@gmail.com>
2021-06-23 00:28:42 -0300

    update appbar and bottomnavigate

--- lib/modules/home/home_page.dart
+++ lib/modules/home/home_page.dart
@@ -1,13 +1,117 @@
 import 'package:flutter/material.dart';
+import 'package:payflow_nlw/modules/home/home_controller.dart';
+import 'package:payflow_nlw/shared/themes/app_colors.dart';
+import 'package:payflow_nlw/shared/themes/app_text_styles.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  _HomePageState createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final controller = HomeController();
+  final pages = [
+    Container(
+      color: Colors.red,
+    ),
+    Container(
+      color: Colors.blue,
+    ),
+  ];
+
+  @override
   Widget build(BuildContext context) {
+    final size = MediaQuery.of(context).size;
+
     return Scaffold(
-      appBar: AppBar(
-        title: Text("HomePage"),
+      appBar: PreferredSize(
+        preferredSize: Size.fromHeight(size.height * 0.2),
+        child: Container(
+          height: size.height * 0.2,
+          color: AppColors.primary,
+          child: Center(
+            child: Padding(
+              padding: const EdgeInsets.only(top: 40, left: 16, right: 16),
+              child: ListTile(
+                title: Text.rich(
+                  TextSpan(
+                    text: 'Olá, ',
+                    style: TextStyles.titleRegular,
+                    children: [
+                      TextSpan(
+                        text: 'Stenio',
+                        style: TextStyles.titleBoldHeading,
+                      ),
+                    ],
+                  ),
+                ),
+                subtitle: Text(
+                  'Mantenha as suas contas em dia',
+                  style: TextStyles.captionShape,
+                ),
+                trailing: Container(
+                  width: size.width * 0.12,
+                  height: size.height * 0.06,
+                  decoration: BoxDecoration(
+                    color: Colors.black,
+                    borderRadius: BorderRadius.circular(5),
+                  ),
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
+      body: pages[controller.currentPage],
+      bottomNavigationBar: Container(
+        height: size.height * 0.10,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            IconButton(
+              onPressed: () {
+                controller.setPage(0);
+                setState(() {});
+              },
+              icon: Icon(Icons.home),
+              color: controller.currentPage == 0
+                  ? AppColors.primary
+                  : AppColors.body,
+            ),
+            SizedBox(
+              width: size.width * 0.07,
+            ),
+            GestureDetector(
+              onTap: () {},
+              child: Container(
+                width: size.width * 0.14,
+                height: size.height * 0.07,
+                decoration: BoxDecoration(
+                    color: AppColors.primary,
+                    borderRadius: BorderRadius.circular(5)),
+                child: Icon(
+                  Icons.add_box_outlined,
+                  color: AppColors.background,
+                ),
+              ),
+            ),
+            SizedBox(
+              width: size.width * 0.07,
+            ),
+            IconButton(
+              onPressed: () {
+                controller.setPage(1);
+                setState(() {});
+              },
+              icon: Icon(Icons.description_outlined),
+              color: controller.currentPage == 1
+                  ? AppColors.primary
+                  : AppColors.body,
+            ),
+          ],
+        ),
       ),
     );
   }
