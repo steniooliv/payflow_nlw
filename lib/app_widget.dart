44438524fb739f23dda49f33957d447dfde14bf4
Stenio Oliveira <steniooliv@gmail.com>
2021-06-24 01:10:30 -0300

    add new routes and block rotate

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -1,15 +1,26 @@
 import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:payflow_nlw/modules/barcode_scanner/barcode_scanner_page.dart';
 import 'package:payflow_nlw/modules/home/home_page.dart';
+import 'package:payflow_nlw/modules/insert_boleto/insert_boleto_page.dart';
 import 'package:payflow_nlw/modules/login/login_page.dart';
 import 'package:payflow_nlw/modules/splash/splash_page.dart';
 import 'package:payflow_nlw/shared/themes/app_colors.dart';
 
 class AppWidget extends StatelessWidget {
+  AppWidget() {
+    SystemChrome.setPreferredOrientations([
+      DeviceOrientation.portraitDown,
+      DeviceOrientation.portraitUp,
+    ]);
+  }
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: 'Pay Flow',
       theme: ThemeData(
+        primarySwatch: Colors.green,
         primaryColor: AppColors.primary,
       ),
       initialRoute: '/splash',
@@ -17,6 +28,8 @@ class AppWidget extends StatelessWidget {
         '/splash': (context) => SplashPage(),
         '/home': (context) => HomePage(),
         '/login': (context) => LoginPage(),
+        '/barcode_scanner': (context) => BarcodeScannerPage(),
+        '/insert_boleto': (context) => InsertBoletoPage(),
       },
     );
   }
