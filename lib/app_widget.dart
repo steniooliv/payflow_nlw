300ec06060c88006306bd268d716fab7cd78c0bc
Stenio Oliveira <steniooliv@gmail.com>
2021-06-26 00:08:12 -0300

    fix boleto field null string

--- lib/app_widget.dart
+++ lib/app_widget.dart
@@ -32,7 +32,7 @@ class AppWidget extends StatelessWidget {
         '/login': (context) => LoginPage(),
         '/barcode_scanner': (context) => BarcodeScannerPage(),
         '/insert_boleto': (context) => InsertBoletoPage(
-              barcode: ModalRoute.of(context) != null
+              barcode: ModalRoute.of(context)!.settings.arguments != null
                   ? ModalRoute.of(context)!.settings.arguments.toString()
                   : null,
             ),
